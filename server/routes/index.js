542df22408d153926a2da970579ce26ec56645b7
rowan <lcf33123@gmail.com>
2025-03-29 15:57:55 +0800

    feat: Add a mock a api

--- server/routes/index.js
+++ server/routes/index.js
@@ -6,4 +6,10 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.post('/api/messages', function(req, res, next) {
+  const { message } = req.body;
+  console.log(message);
+  res.json({ message: 'Hello, how are you?' });
+});
+
 module.exports = router;
